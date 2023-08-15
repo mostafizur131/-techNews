4d828281a3544f86fc002b3849c68bd2185b5107
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-08-15 22:51:43 +0600

    all page creation done

---
+++ src/components/Menu.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+const Menu = () => {
+  return (
+    <header className="p-4 bg-gray-800 text-gray-100">
+      <div className="container flex justify-between h-10 mx-auto">
+        <Link
+          href="/"
+          aria-label="Back to homepage"
+          className="flex items-center p-2 text-2xl font-bold"
+        >
+          Tech News
+        </Link>
+        <ul className="items-stretch hidden space-x-3 md:flex">
+          <li className="flex">
+            <Link
+              href="/"
+              className="flex items-center px-4 -mb-1 border-b-2 border-transparent"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="flex">
+            <Link
+              href="/blog"
+              className="flex items-center px-4 -mb-1 border-b-2 border-transparent"
+            >
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Menu;
